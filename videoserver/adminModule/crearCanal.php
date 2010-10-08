8c9fc3f8865fcc7b0277f0b180122ad959ac688e
John Aristizabal <jraristi@gmail.com>
2010-10-08 10:22:58 -0500

    Primer Commit

---
+++ videoserver/adminModule/crearCanal.php
@@ -0,0 +1,36 @@
+<html>
+
+  <head>
+    <link rel="stylesheet" href="../styles/adminStyles.css">
+    <meta name="tipo_contenido"  content="text/html;" http-equiv="content-type" charset="utf-8">
+  </head>
+
+  <body>
+
+<!-- Site navigation menu -->
+    <ul class="navbar">
+      <li><a href="crearCanal.php">Crear canal</a>
+      <li><a href="musings.html">Agregar programas</a>
+      <li><a href="town.html">Usuarios registrados</a>
+      <li><a href="links.html">Cambio de contraseña</a>
+    </ul>
+
+<!-- Main content -->
+    <h1>Módulo administrador: creación de canal.</h1>
+
+    <p>Welcome to my styled page!
+
+<p>It lacks images, but at least it has style.
+And it has links, even if they don't go
+anywhere&hellip;</p>
+
+<p>There should be more here, but I don't know
+what yet.</p>
+
+<!-- Sign and date the page, it's only polite! -->
+<address>Made 5 April 2004<br>
+  by myself.</address>
+
+  </body>
+
+</html>
